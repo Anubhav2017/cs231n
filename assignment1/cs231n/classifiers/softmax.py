411a3776dce1a0ba8b234dda920e8c1cfef14dd8
Anubhav2017 <35400467+Anubhav2017@users.noreply.github.com>
2018-12-29 12:13:19 +0530

    Add files via upload

---
+++ assignment1/cs231n/classifiers/softmax.py
@@ -0,0 +1,91 @@
+import numpy as np
+from random import shuffle
+
+def softmax_loss_naive(W, X, y, reg):
+  """
+  Softmax loss function, naive implementation (with loops)
+
+  Inputs have dimension D, there are C classes, and we operate on minibatches
+  of N examples.
+
+  Inputs:
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
+  - reg: (float) regularization strength
+
+  Returns a tuple of:
+  - loss as single float
+  - gradient with respect to weights W; an array of same shape as W
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  N=X.shape(0)
+  dW = np.zeros_like(W)
+  yp=X.dot(W)
+  yp=np.exp(yp)
+  ysum=np.sum(yp,axis=1)
+  probs=np.divide(yp,ysum) 
+  losses=-log(probs[np.arange(0,N),y])
+  loss=np.sum(losses,axis=0)
+  loss/=N
+  loss+=0.5*reg*np.sum(W*W)
+  da=probs[np.arange(0,N),y]-1
+  dW=X.T.da
+  dW+=reg*W
+
+
+
+
+
+
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return loss, dW
+
+
+def softmax_loss_vectorized(W, X, y, reg):
+  """
+  Softmax loss function, vectorized version.
+
+  Inputs and outputs are the same as softmax_loss_naive.
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  N=X.shape(0)
+  dW = np.zeros_like(W)
+  yp=X.dot(W)
+  yp=np.exp(yp)
+  ysum=np.sum(yp,axis=1)
+  probs=np.divide(yp,ysum) 
+  losses=-log(probs[np.arange(0,N),y])
+  loss=np.sum(losses,axis=0)
+  loss/=N
+  loss+=0.5*reg*np.sum(W*W)
+  da=probs[np.arange(0,N),y]-1
+  dW=X.T.da
+  dW+=reg*W
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return loss, dW
+
